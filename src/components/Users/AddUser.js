b337be43aa22d380c670bb2ac78ae0c2886be0b4
Walace Andrade Matos <walace.andr.m@gmail.com>
2023-09-24 20:38:14 -0300

    Refactored components based on course.

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" step="1"></input>
+        <Button buttonText="Add User" buttonType="submit" />
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
