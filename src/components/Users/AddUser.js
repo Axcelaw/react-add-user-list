b006ead6c0b6c79ee2d4839b00d0cb755016b141
Walace Andrade Matos <walace.andr.m@gmail.com>
2023-09-25 10:40:39 -0300

    Added username and age states on AddUser component.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = () => {
+  const [nameState, setNameState] = useState("");
+  const [ageState, setAgeState] = useState(0);
+
+  const nameChangeHandler = (event) => {
+    setNameState(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAgeState(event.target.value);
+  };
+
   const submitHandler = (event) => {
     event.preventDefault();
   };
@@ -11,9 +23,20 @@ const AddUser = () => {
     <Card className={classes.input}>
       <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          value={nameState}
+          onChange={nameChangeHandler}
+        ></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" step="1"></input>
+        <input
+          id="age"
+          type="number"
+          step="1"
+          value={ageState}
+          onChange={ageChangeHandler}
+        ></input>
         <Button buttonType="submit">Add User</Button>
       </form>
     </Card>
